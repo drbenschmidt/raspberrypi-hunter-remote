2ee46c3a52524129f9a1c627c7b7d999aa0d4d04
drbenschmidt <dr.ben.schmidt@gmail.com>
2020-06-04 21:04:40 -0500

    skeletonzzz

--- raspberrypi_hunter_remote/module.py
+++ raspberrypi_hunter_remote/module.py
@@ -1,31 +1,46 @@
-"""
-Module provides a simple cubic_rectification function.
-"""
+import flask
+from flask import request, jsonify
 
-import numpy as np
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
+# [GET] / => home
+# [GET] /fans => list fan ids
+# [GET] /fans/{id} => get fan info
+# [PATCH] /fans/{id} => set values
 
-def cubic_rectification(x):
-    '''
-    Rectified cube of an array.
+local_fans = [
+    {
+        "id": 1,
+        "name": "Downstairs Fan"
+    }, {
+        "id": 2,
+        "name": "Bedroom Fan"
+    }
+]
 
-    Parameters
-    ----------
-    X : numpy.ndarray
-        Input array.
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-    Returns
-    -------
-    numpy.ndarray
-        Elementwise, the cube of `X` where it is positive and `0` otherwise.
+@app.route('/fans', methods=['GET'])
+def fans():
+    return jsonify(local_fans)
 
-    Note
-    ----
-    This is a sample function, using a Google docstring format.
+@app.route('/fans/<int:id>', methods=['GET'])
+def fans_id(id):
+    results = []
 
-    Note
-    ----
-    The use of intersphinx will cause numpy.ndarray above to link to its
-    documentation, but not inside this Note.
-    '''
-    return np.maximum(0, x**3)
+    for fan in local_fans:
+        if (fan['id'] == id):
+            results.append(fan)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+@app.route('/fans/<int:id>', methods=['PATCH'])
+def fans_id_patch(id):
+    return jsonify(request.form)
+
+app.run()
